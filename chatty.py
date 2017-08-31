caade5d786c44d53d95c519e43de673d32c2dc9c
gentom <gento.m.developer@gmail.com>
2017-08-31 18:31:40 +0900

    commit

--- chatty.py
+++ chatty.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -19,10 +19,9 @@ def event_handler(json):
     print('Recived event: '+str(json))
     socketio.emit('response', json, callback=ack)
 
-@socketio.on('msg')
-def msg_handler(msg):
-    print('message: ' + msg)
-    send(msg, broadcast=True)
+@socketio.on('new')
+def msg_handler(newusr):
+    socketio.emit('joined',newusr, callback=ack)
 
 if __name__ == '__main__':
     socketio.run(app, port=5001, debug=True)
