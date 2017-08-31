26f69f07e1e105645f0abf301b1c73687bd686fe
gentom <gento.m.developer@gmail.com>
2017-08-31 16:52:46 +0900

    commit

--- chatty.py
+++ chatty.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
-# coding: utf-8
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
+
+@app.route('/')
+def render():
+    return render_template('./chatty.html')
+
+@socketio.on('message')
+def handle_msg(msg):
+    print('Message: '+ msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
